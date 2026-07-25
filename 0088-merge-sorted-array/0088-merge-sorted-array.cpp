5494c3fecd700f337596401a37c09d2a30b89a22
prakshal0325 <prakshal0325@gmail.com>
2026-07-25 16:08:13 +0530

    Time: 0 ms (100%), Space: 12.5 MB (19.12%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        vector<int> temp;
+        vector<int> temp;
 
-        int i = 0, j = 0;
+        int i = 0, j = 0;
 
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                temp.push_back(nums1[i]);
-                i++;
-            } else {
-                temp.push_back(nums2[j]);
-                j++;
-            }
-        }
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp.push_back(nums1[i]);
+                i++;
+            } else {
+                temp.push_back(nums2[j]);
+                j++;
+            }
+        }
 
-        while (i < m) {
-            temp.push_back(nums1[i]);
-            i++;
-        }
+        while (i < m) {
+            temp.push_back(nums1[i]);
+            i++;
+        }
 
-        while (j < n) {
-            temp.push_back(nums2[j]);
-            j++;
-        }
+        while (j < n) {
+            temp.push_back(nums2[j]);
+            j++;
+        }
 
-        // Copy back to nums1
-        for (int k = 0; k < m + n; k++) {
-            nums1[k] = temp[k];
-        }
-    }
+        // Copy back to nums1
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = temp[k];
+        }
+    }
 };

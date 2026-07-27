3d6d617b5a8c626fce985a5411dc94bae3cfe86d
prakshal0325 <prakshal0325@gmail.com>
2026-07-27 22:26:46 +0530

    Time: 5 ms (30.7%), Space: 9 MB (44.23%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -3,6 +3,12 @@ public:
     bool isPowerOfThree(int n) {
           if (n <= 0)
             return false;
-            return ((n==1) || (( n % 3 == 0) && isPowerOfThree(n/3)));
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
     }
+    
 };

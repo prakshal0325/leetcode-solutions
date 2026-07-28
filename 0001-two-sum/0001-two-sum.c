414d31c91b4cf20c8db2a7bc7096a2f626953563
prakshal0325 <prakshal0325@gmail.com>
2026-07-28 15:59:52 +0530

    Time: 0 ms (100%), Space: 10.7 MB (5.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,24 +1,36 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    struct hashTable {
+        int key;
+        int value;
+        UT_hash_handle hh;
+    } *hashTable = NULL, *item;
 
     for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
+        int complement = target - nums[i];
 
-            if (nums[i] + nums[j] == target) {
+        HASH_FIND_INT(hashTable, &complement, item);
 
-                int* ans = (int*)malloc(2 * sizeof(int));
-                ans[0] = i;
-                ans[1] = j;
+        if (item) {
+            int* result = (int*)malloc(sizeof(int) * 2);
+            result[0] = item->value;
+            result[1] = i;
+            *returnSize = 2;
 
-                *returnSize = 2;
-                return ans;
-            }
+            HASH_CLEAR(hh, hashTable);   // Free the hash table
+            return result;
         }
+
+        item = (struct hashTable*)malloc(sizeof(struct hashTable));
+        item->key = nums[i];
+        item->value = i;
+
+        HASH_ADD_INT(hashTable, key, item);
     }
 
     *returnSize = 0;
-    return NULL;
+    HASH_CLEAR(hh, hashTable);   // Free the hash table
+
+    return malloc(0);   // Return an empty array if no solution is found
 }
